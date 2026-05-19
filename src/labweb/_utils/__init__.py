7c5021ee4ce0ec412de4ecd5fb2b7e838921740c
Jonatas <joncortes.on@gmail.com>
2026-05-19 11:01:13 -0300

    FEAT: add function camel_case_to_snake_case to _utils module

--- src/labweb/_utils/__init__.py
+++ src/labweb/_utils/__init__.py
@@ -1,6 +1,8 @@
 from ._geometry import point_to_segment_distance, is_inside_circle
+from ._regex import camel_case_to_snake_case
 
 __all__ = [
     "point_to_segment_distance",
-    "is_inside_circle"
+    "is_inside_circle",
+    "camel_case_to_snake_case"
 ]
